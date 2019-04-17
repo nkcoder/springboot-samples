c6596b5019879024014549eeb53c4b1b729a78be
daniel <daniel5hbs@gmail.com>
2019-04-17 10:01:25 +0800

    add application-integration.properties

--- tdd-demo/src/test/java/org/nkcoder/IntegrationTest.java
+++ tdd-demo/src/test/java/org/nkcoder/IntegrationTest.java
@@ -11,10 +11,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-integration.properties")
 public class IntegrationTest {
 
   @Autowired
