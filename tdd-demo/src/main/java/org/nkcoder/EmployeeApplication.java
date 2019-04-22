3a26f8188fa8592aecc0f2307779f5260e58655a
daniel <daniel5hbs@gmail.com>
2019-04-22 13:01:35 +0800

    remove spring cloud config

--- tdd-demo/src/main/java/org/nkcoder/EmployeeApplication.java
+++ tdd-demo/src/main/java/org/nkcoder/EmployeeApplication.java
@@ -2,6 +2,7 @@ package org.nkcoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class EmployeeApplication {
