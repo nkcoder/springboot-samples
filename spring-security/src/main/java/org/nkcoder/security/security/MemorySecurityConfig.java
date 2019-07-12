d424cd15b1d4fe2a1b5fadf1fcb2cb90c47ab776
daniel <daniel5hbs@gmail.com>
2019-07-12 17:06:30 +0800

    spring security: add password encoder

--- spring-security/src/main/java/org/nkcoder/security/security/MemorySecurityConfig.java
+++ spring-security/src/main/java/org/nkcoder/security/security/MemorySecurityConfig.java
@@ -1,11 +1,14 @@
 package org.nkcoder.security.security;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,11 +19,11 @@ public class MemorySecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
         .withUser("kobe")
-        .password("{noop}123456")
+        .password(passwordEncoder().encode("123456"))
         .authorities("USER")
         .and()
         .withUser("durant")
-        .password("{noop}123456")
+        .password(passwordEncoder().encode("123456"))
         .roles("ADMIN")
         .authorities("USER");
   }
@@ -42,7 +45,11 @@ public class MemorySecurityConfig extends WebSecurityConfigurerAdapter {
 
         .and()
         .headers().frameOptions().sameOrigin();
+  }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
