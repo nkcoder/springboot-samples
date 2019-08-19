fc657e927464b82e37e5173fa1986d2bf1b2667a
daniel <daniel5hbs@gmail.com>
2019-08-19 23:01:09 +0800

    #spring-data-jpa: add database sql

--- spring-data-jpa/src/main/resources/db/migration/V1.0__init-tables.sql
+++ spring-data-jpa/src/main/resources/db/migration/V1.0__init-tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS player
     id      INT AUTO_INCREMENT,
     name    VARCHAR(64) NOT NULL,
     team_id INT         NOT NULL,
-    join_at TIMESTAMP,
+    join_at DATE,
     PRIMARY KEY (id)
 ) ENGINE = INNODB;
 
@@ -12,6 +12,6 @@ CREATE TABLE IF NOT EXISTS team
     id         INT AUTO_INCREMENT,
     name       VARCHAR(64) NOT NULL,
     location   varchar(128) DEFAULT '',
-    founded_at TIMESTAMP,
+    founded_at DATE ,
     PRIMARY KEY (id)
 ) ENGINE = INNODB;
